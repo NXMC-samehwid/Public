77dcb458920ce2568ab67ce89d277f13bdd45a48
dgbaodev2407 <dgbaopro2407@gmail.com>
2026-01-27 13:31:47 +0700

    update UI

---
+++ installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd
+if [ -e "/data/data/com.termux/files/home/storage" ]; then
+	rm -rf /data/data/com.termux/files/home/storage
+fi
+termux-setup-storage
+yes | pkg update
+. <(curl https://cdn.quanghuynopro.com/store/termux-change-repo.sh)
+yes | pkg upgrade
+yes | pkg i python
+yes | pkg i python-pip
+yes | pkg i git xdelta3 wget
+
+wget -O ~/NXMC_INSTALLER https://github.com/NXMC-samehwid/Public/releases/download/base-release/installer
+chmod +x ~/NXMC_INSTALLER
+$(realpath ~/NXMC_INSTALLER)
